58e75d85611b875ce328d396e73e0a38f9664af7
mcsarmend <mcsarmend@gmail.com>
2022-02-18 17:27:22 -0600

    Primera Etapa Pinterest App

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,8 @@
 //import 'package:disenos_app/src/pages/headers_page.dart';
 //import 'src/pages/graficas_circulares_pages.dart';
 //import 'package:disenos_app/src/labs/slideshow_page.dart';
-import 'package:disenos_app/src/pages/slideshow_page.dart';
+//import 'package:disenos_app/src/pages/slideshow_page.dart';
+import 'package:disenos_app/src/pages/pinterest_page.dart';
 import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
     return    MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños App',
-      home: SlideshowPage(),
+      home: PinterestPage(),
     );
   }
 }
